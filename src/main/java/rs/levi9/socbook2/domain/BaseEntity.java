2fe40e169d9bf3311def40cb26fb68cc2ebd6728
zoricab <zorka0000@gmail.com>
2017-07-18 10:41:38 +0200

    added role and user domains

--- src/main/java/rs/levi9/socbook2/domain/BaseEntity.java
+++ src/main/java/rs/levi9/socbook2/domain/BaseEntity.java
@@ -1,5 +1,18 @@
 package main.java.rs.levi9.socbook2.domain;
 
+@MappedSuperclass
 public class BaseEntity {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 }
--- src/main/java/rs/levi9/socbook2/domain/User.java
+++ src/main/java/rs/levi9/socbook2/domain/User.java
@@ -1,5 +1,42 @@
 package main.java.rs.levi9.socbook2.domain;
 
-public class User {
+import java.util.Collection;
 
+public class User {
+	
+	private String username;
+	private String email;
+	private boolean status;
+	private String password;
+	private Collection<? extends GrantedAuthority> roles;
+	
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmaill() {
+		return email;
+	}
+	public void setEmaill(String emaill) {
+		this.email = emaill;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
