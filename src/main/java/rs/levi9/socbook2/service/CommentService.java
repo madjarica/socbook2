9604c38c262ef72193a74f66f1cf243d8d82b295
zoricab <zorka0000@gmail.com>
2017-07-25 14:15:56 +0200

    added bookmark details frontend page, comment repository, service and controller

---
+++ src/main/java/rs/levi9/socbook2/service/CommentService.java
@@ -0,0 +1,36 @@
+package rs.levi9.socbook2.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import rs.levi9.socbook2.domain.Comment;
+import rs.levi9.socbook2.repository.CommentRepository;
+
+@Service
+public class CommentService {
+
+private CommentRepository commentRepository;
+	
+	@Autowired
+	public CommentService(CommentRepository commentRepository){
+		this.commentRepository = commentRepository;
+	}
+	
+	public Comment findOne(Long id){
+		return commentRepository.findOne(id);
+	}
+	
+	public List<Comment> findAll(){
+		return commentRepository.findAll();
+	}
+	
+	public void delete(Long id){
+		commentRepository.delete(id);
+	}
+	
+	public Comment save(Comment comment){
+		return commentRepository.save(comment);
+	}
+}
---
+++ src/main/java/rs/levi9/socbook2/web/controller/CommentController.java
@@ -0,0 +1,54 @@
+package rs.levi9.socbook2.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import rs.levi9.socbook2.domain.Comment;
+import rs.levi9.socbook2.service.CategoryService;
+import rs.levi9.socbook2.service.CommentService;
+
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+
+	private CommentService commentService;
+	
+	@Autowired
+	public CommentController(CommentService commentService){
+		this.commentService = commentService;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Comment> findAll(){
+		return commentService.findAll();
+	}
+	
+	@RequestMapping(path = "{id}", method = RequestMethod.GET)
+	public Comment findOne(@PathVariable("id") Long id){
+		return commentService.findOne(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public Comment save(@RequestBody Comment comment){
+		return commentService.save(comment);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT)
+	public Comment update(@RequestBody Comment comment){
+		return commentService.save(comment);
+	}
+	
+	@RequestMapping(path="{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable("id") Long id){
+		commentService.delete(id);
+	}
+	
+	
+}
